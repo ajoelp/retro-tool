0329356e13fca160eafb13c53cf60894a834fc72
Joel Podrebarac <joelpodrebar@gmail.com>
2021-08-16 14:34:18 -0400

    Fix code coverage

--- apps/api/src/utils/generatePath.ts
+++ apps/api/src/utils/generatePath.ts
@@ -1,27 +1,10 @@
 import { compile } from 'path-to-regexp';
 
-const cache = {};
-const cacheLimit = 10000;
-let cacheCount = 0;
-
-function compilePath(path: string) {
-  if (cache[path]) return cache[path];
-
-  const generator = compile(path);
-
-  if (cacheCount < cacheLimit) {
-    cache[path] = generator;
-    cacheCount++;
-  }
-
-  return generator;
-}
-
-/**
- * Public API for generating a URL pathname from a path and parameters.
- */
-function generatePath(path = '/', params: Record<string, string> = {}) {
-  return path === '/' ? path : compilePath(path)(params, { pretty: true });
+function generatePath(
+  path: string,
+  params: Record<string, string | number> = {},
+) {
+  return path === '/' ? path : compile(path)(params);
 }
 
 export default generatePath;
