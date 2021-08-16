cb949ce459468e16a729cdf3e8e1705a7378f9cb
Tyler Bauman <t.bauman@vehikl.com>
2021-08-16 10:23:47 -0400

    Layout changes

--- apps/client/src/app/Nav.tsx
+++ apps/client/src/app/Nav.tsx
@@ -1,8 +1,14 @@
+import { PlayIcon } from '@heroicons/react/solid';
+
 const Navbar = () => {
   return (
     <div className="w-full h-16 border-b grid grid-cols-12 items-center p-4">
       <div className="col-span-4">
         <p className="text-xl">Title</p>
+      </div>
+      <div className="flex items-center">
+        <p className="mr-2">5:00</p>
+        <PlayIcon className="w-7 h-7 text-green-500" />
       </div>
     </div>
   )
