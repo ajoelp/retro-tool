1dab7ad693bfb2177a9856140b0694e4d09886c0
Joel Podrebarac <joelpodrebar@gmail.com>
2021-08-16 16:03:57 -0400

    Use prisma migrate to refresh database

--- apps/api/prisma-test-environment.ts
+++ apps/api/prisma-test-environment.ts
@@ -1,107 +1,46 @@
 import path from 'path';
-import util from 'util';
 import NodeEnvironment from 'jest-environment-node';
-import childProcess from 'child_process';
-import mysql, { Connection } from 'mysql';
 import { prisma } from './src/prismaClient';
+import {ensureDatabaseExists} from "@prisma/migrate/dist/utils/ensureDatabaseExists";
+import {Migrate} from "@prisma/migrate";
 
 const schemaPath = path.resolve(__dirname, '../../', 'prisma', 'schema.prisma');
 
-const exec = util.promisify(childProcess.exec);
-
-const databaseUser = process.env.TEST_DB_USER ?? 'root';
-const databasePassword = process.env.TEST_DB_PASSWORD ?? 'password';
-const databaseHost = process.env.TEST_DATABASE_HOST ?? 'localhost'
-const databasePort = parseInt(process.env.TEST_DATABASE_PORT ?? '3308')
-
-const prismaBinary = path.join(
-  __dirname,
-  '../../',
-  'node_modules',
-  '.bin',
-  'prisma',
-);
-
 class PrismaTestEnvironment extends NodeEnvironment {
-  private connection: Connection;
-  private databaseName: string;
 
   constructor(config) {
     super(config);
-
-    this.connection = mysql.createConnection({
-      host: databaseHost,
-      user: databaseUser,
-      password: databasePassword,
-      port: databasePort,
-    });
-
-    this.databaseName = `test_db_${(Math.random() + 1)
-      .toString(36)
-      .substring(7)}`;
-
-    process.env.DATABASE_URL = `mysql://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${this.databaseName}?connection_limit=5`;
-    this.global.process.env.DATABASE_URL = `mysql://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${this.databaseName}?connection_limit=5`;
+    process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+    this.global.process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
   }
 
-  connect() {
-    return new Promise((resolve, reject) => {
-      const connection = this.connection;
-      connection.connect(function (error) {
-        if (error) return reject(error);
-        resolve(connection.threadId);
-      });
-    });
-  }
+  async push() {
+    const migrate = new Migrate(schemaPath)
 
-  disconnect() {
-    return new Promise((resolve, reject) => {
-      const connection = this.connection;
-      connection.end(function (error) {
-        if (error) return reject(error);
-        resolve(connection.threadId);
-      });
-    });
-  }
+    await ensureDatabaseExists('push', true, schemaPath)
 
-  createDatabase() {
-    return new Promise((resolve, reject) => {
-      const connection = this.connection;
-      connection.query(
-        `CREATE DATABASE ${this.databaseName};`,
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        },
-      );
-    });
-  }
+    try {
+      await migrate.reset()
+    } catch (e) {
+      migrate.stop()
+      throw e
+    }
 
-  dropDatabase() {
-    return new Promise((resolve, reject) => {
-      const connection = this.connection;
-      connection.query(
-        `DROP DATABASE IF EXISTS ${this.databaseName};`,
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        },
-      );
-    });
+    try {
+      await migrate.push({ force: true })
+    } catch (e) {
+      migrate.stop()
+      throw e
+    }
   }
 
   async setup() {
-    // Run the migrations to ensure our schema has the required structure
-    await this.connect();
-    await this.createDatabase();
-    await exec(`${prismaBinary} db push --schema=${schemaPath}`);
+    await this.push()
     return super.setup();
   }
 
   async teardown() {
     try {
-      await this.dropDatabase();
-      await this.disconnect();
       await prisma.$disconnect();
     } catch (error) {
       // doesn't matter as the environment is torn down
