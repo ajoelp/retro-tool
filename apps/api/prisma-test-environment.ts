f1c3c89503855ceae440f2acc40d7a359a7bfde6
Joel Podrebarac <joel@pop-os.localdomain>
2021-08-18 09:22:20 -0400

    Switch to postgres and schemas

--- apps/api/prisma-test-environment.ts
+++ apps/api/prisma-test-environment.ts
@@ -1,46 +1,51 @@
 import path from 'path';
 import NodeEnvironment from 'jest-environment-node';
 import { prisma } from './src/prismaClient';
-import {ensureDatabaseExists} from "@prisma/migrate/dist/utils/ensureDatabaseExists";
-import {Migrate} from "@prisma/migrate";
+import {nanoid} from "nanoid";
+import childProcess from 'child_process'
+import * as util from "util";
+import { Client } from 'pg'
+
+const exec = util.promisify(childProcess.exec)
 
 const schemaPath = path.resolve(__dirname, '../../', 'prisma', 'schema.prisma');
 
+const prismaBinary = path.join(
+  __dirname,
+  '../../',
+  'node_modules',
+  '.bin',
+  'prisma',
+)
+
+
 class PrismaTestEnvironment extends NodeEnvironment {
+  private schemaName: string;
+  private connection: any;
 
   constructor(config) {
     super(config);
-    process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
-    this.global.process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+    this.schemaName = `test_${nanoid()}`;
+    this.connection = process.env.TEST_DATABASE_URL.replace('schema=public', `schema=${this.schemaName}` )
+    process.env.DATABASE_URL = this.connection;
+    this.global.process.env.DATABASE_URL = this.connection;
   }
 
-  async push() {
-    const migrate = new Migrate(schemaPath)
-
-    await ensureDatabaseExists('push', true, schemaPath)
-
-    try {
-      await migrate.reset()
-    } catch (e) {
-      migrate.stop()
-      throw e
-    }
-
-    try {
-      await migrate.push({ force: true })
-    } catch (e) {
-      migrate.stop()
-      throw e
-    }
-  }
 
   async setup() {
-    await this.push()
+    await exec(`${prismaBinary} db push --schema=${schemaPath}`)
     return super.setup();
   }
 
   async teardown() {
+    const client = new Client({
+      connectionString: this.connection
+    })
+
     try {
+      await client.connect()
+      await client.query(`DROP SCHEMA IF EXISTS "${this.schemaName}" CASCADE`);
+      await client.end();
       await prisma.$disconnect();
     } catch (error) {
       // doesn't matter as the environment is torn down
