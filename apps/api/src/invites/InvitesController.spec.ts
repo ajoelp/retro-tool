564b2aa3c568e1334dfb7468d6e43c45fcc59f3f
Vehikl <go@vehikl.com>
2021-09-29 16:43:13 -0400

    Add board invite landing page and route

--- apps/api/src/invites/InvitesController.spec.ts
+++ apps/api/src/invites/InvitesController.spec.ts
@@ -32,4 +32,27 @@ describe('InvitesController', () => {
     });
     expect(access).toBeTruthy();
   });
+
+  it('throws when a board does not exist', async () => {
+    const user = await prisma.user.create({
+      data: { githubNickname: 'nacho', email: 'nacho@gmail.com', avatar: '' },
+    });
+
+    const initialAccess = await prisma.boardAccess.findFirst({
+      where: { userId: user.id, boardId: 'invalid-board-id' },
+    });
+    expect(initialAccess).toBeFalsy();
+
+    const response = await TestCase.make()
+      .actingAs(user)
+      .post(`/invites/asdasd`);
+
+    expect(response.status).toEqual(404);
+  });
+
+  afterEach(async () => {
+    await prisma.boardAccess.deleteMany();
+    await prisma.board.deleteMany();
+    await prisma.user.deleteMany();
+  });
 });
--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,11 +1,22 @@
-import { BrowserRouter as Router, Link, Switch, Route as BaseRoute, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route as BaseRoute,
+  Redirect,
+} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ChakraProvider, Spinner } from '@chakra-ui/react';
 import { DialogManager } from './dialog-manager';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { AuthProvider, useAuth } from './contexts/AuthProvider';
 import { theme } from './theme';
-import { lazy, LazyExoticComponent, ReactComponentElement, Suspense } from 'react';
+import {
+  lazy,
+  LazyExoticComponent,
+  ReactComponentElement,
+  Suspense,
+} from 'react';
 import '@fontsource/inter';
 import { IgnoredEventsProvider } from './contexts/IgnoredEventsContext';
 
@@ -21,24 +32,28 @@ type AuthenticatedRouteProps = {
   path: string;
   auth?: boolean;
   exact?: boolean;
-  component: LazyExoticComponent<any>
-}
+  component: LazyExoticComponent<any>;
+};
 
 const Route = (props: AuthenticatedRouteProps) => {
-  const { path, exact = false, component: Component, auth = false } = props
-  const { user } = useAuth()
+  const { path, exact = false, component: Component, auth = false } = props;
+  const { user } = useAuth();
   return (
-    <BaseRoute path={path} exact={exact} render={({ location }) => {
-      if (auth && !user) return <Redirect to={`/?redirect=${location.pathname}`} />
-      return (
-        <Suspense fallback={<Spinner />}>
-          <Component />
-        </Suspense>
-      )
-    }} />
-  )
-}
-
+    <BaseRoute
+      path={path}
+      exact={exact}
+      render={({ location }) => {
+        if (auth && !user)
+          return <Redirect to={`/?redirect=${location.pathname}`} />;
+        return (
+          <Suspense fallback={<Spinner />}>
+            <Component />
+          </Suspense>
+        );
+      }}
+    />
+  );
+};
 
 export const App = () => {
   return (
@@ -49,8 +64,21 @@ export const App = () => {
             <AuthProvider>
               <Router>
                 <Switch>
-                  <Route path="/" exact={true} component={lazy(() => import('./pages/Landing'))} />
-                  <Route path="/boards/:id" auth={true} component={lazy(() => import('./pages/Board'))} />
+                  <Route
+                    path="/"
+                    exact={true}
+                    component={lazy(() => import('./pages/Landing'))}
+                  />
+                  <Route
+                    path="/boards/:id"
+                    auth={true}
+                    component={lazy(() => import('./pages/Board'))}
+                  />
+                  <Route
+                    path="/invites/:inviteCode"
+                    auth={true}
+                    component={lazy(() => import('./pages/Invite'))}
+                  />
                 </Switch>
               </Router>
             </AuthProvider>
